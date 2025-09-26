0bd0b58115201dc6d5043722ac36ca1074127e01
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2025-09-26 13:46:46 +0200

    Create AddIntegers class to do exercise 1

---
+++ src/com/olejarczykjakub/exercise_1/AddIntegers.java
@@ -0,0 +1,12 @@
+package com.olejarczykjakub.exercise_1;
+
+public class AddIntegers {
+
+	public static void main(String[] args) {
+		int num1 = Integer.parseInt(args[0]);
+		int num2 = Integer.parseInt(args[1]);
+		int sum = num1 + num2;
+		System.out.println(sum);
+	}
+
+}
